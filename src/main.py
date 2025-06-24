4bc14140b7737d0dff88f44220f43677b64521e7
SY <ArchdukeArcoti@gmail.com>
2025-06-24 17:15:43 +0800

    Minor updates

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from .benchmark.rastrigin import Rastrigin
+from .benchmark.ackley import Ackley
 from .PSO import particle_swarm_optimisation
 
 if __name__ == "__main__":
     n = 2
-    rastrigin = Rastrigin(n)
-    result = particle_swarm_optimisation(n, rastrigin, verbose=True)
+    ackley = Ackley(n)
+    result = particle_swarm_optimisation(n, ackley, verbose=True)
     print(result)
