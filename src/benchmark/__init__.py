b01419831930ec1b0f239cdc01f0abef05c19367
SY <ArchdukeArcoti@gmail.com>
2025-06-25 10:23:01 +0800

    Allow import all

---
+++ src/benchmark/__init__.py
@@ -0,0 +1,8 @@
+from .ackley import Ackley
+from .griewank import Griewank
+from .michalewicz import Michalewicz
+from .rastrigin import Rastrigin
+from .rosenbrock import Rosenbrock
+from .sphere import Sphere
+
+__all__ = ["Ackley", "Griewank", "Michalewicz", "Rastrigin", "Rosenbrock", "Sphere"]
--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-from .benchmark.sphere import Sphere
+from .benchmark import *
 from .PSO import particle_swarm_optimisation
 
 if __name__ == "__main__":
     n = 2
-    sphere = Sphere(n)
-    result = particle_swarm_optimisation(n, sphere, verbose=True)
+    result = particle_swarm_optimisation(n, Michalewicz(n), verbose=True)
     print(result)
