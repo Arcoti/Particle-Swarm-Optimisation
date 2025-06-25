dc9fcb70e8a4e9eefd5636573e9c35b2ee5d6d42
SY <ArchdukeArcoti@gmail.com>
2025-06-25 10:21:17 +0800

    Add  michalewics function

---
+++ src/benchmark/michalewicz.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from .base import BaseTestFunction
+
+class Michalewicz(BaseTestFunction):
+    """
+    The Michalewicz Function.
+    """
+    def __init__(self, n: int, domain: tuple[float, float] = (0, np.pi)):
+        self._n = n
+        self._domain = domain
+
+    def __call__(self, x: np.ndarray):
+        if x.size != self._n:
+            raise ValueError(f"Input dimension {x.size} does not match expected {self._n}")
+        
+        i = np.arange(1, x.size + 1)
+        return - np.sum( np.sin(x) * ( np.sin((i * x**2) / np.pi) )**2 )
+    
+    @property
+    def global_minimum(self):
+        return np.full(self._n, np.pi), -self._n
+    
+    @property
+    def domain(self):
+        return self._domain
+    
+    def within_domain(self, x):
+        return np.all( (x >= self._domain[0]) & (x <= self._domain[1] ) )
+    
