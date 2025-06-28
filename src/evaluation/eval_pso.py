f9591a83a55b4ad4fe62d3666fa372d625133065
Arcoti <ArchdukeArcoti@gmail.com>
2025-06-28 12:43:28 +0800

    Added rough plan for PSO eval

---
+++ src/evaluation/eval_pso.py
@@ -0,0 +1,5 @@
+# PSO Function for evaluation
+
+# Calculate Average Loss (average fitness of each particle) then find relative error
+# Success within a range of the technical minimum
+# Convergence iterations
